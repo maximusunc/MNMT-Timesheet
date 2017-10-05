3abd34450d2c6fcd84f744cfc8c000d9d6a03f13
Max Wang <maxwellswang@gmail.com>
2017-10-05 19:12:08 -0400

    more adjustments to javascript

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,38 @@
 // Capture Button Click
-$("#add-user").on("click", function(event) {
-  // prevent form from trying to submit/refresh the page
-  event.preventDefault();
+  $("#add-user").on("click", function(event) {
+    // prevent page from refreshing when form tries to submit itself
+    event.preventDefault();
 
-  // Capture User Inputs and store them into variables
-  var name = $("#name-input").val().trim();
-  var email = $("#email-input").val().trim();
-  var age = $("#age-input").val().trim();
-  var comment = $("#comment-input").val().trim();
+    // Capture user inputs and store them into variables
+    var name = $("#name-input").val().trim();
+    var email = $("#email-input").val().trim();
+    var age = $("#age-input").val().trim();
+    var comment = $("#comment-input").val().trim();
 
-  // Console log each of the user inputs to confirm we are receiving them
-  console.log(name);
-  console.log(email);
-  console.log(age);
-  console.log(comment);
+    // Console log each of the user inputs to confirm we are receiving them
+    console.log(name);
+    console.log(email);
+    console.log(age);
+    console.log(comment);
 
-  // Output all of the new information into the relevant HTML sections
-  $("#name-display").html(name);
-  $("#email-display").html(email);
-  $("#age-display").html(age);
-  $("#comment-display").html(comment);
+    // Replaces the content in the "recent-member" div with the new info
+    $("#name-display").text(name);
+    $("#email-display").text(email);
+    $("#age-display").text(age);
+    $("#comment-display").text(comment);
 
-});
+    // Clear localStorage
+    localStorage.clear();
 
-localStorage.setItem("name", name);
-  localStorage.setItem("email",email);
-  localStorage.setItem("age", age);
-  localStorage.setItem("comment", comment);
+    // Store all content into localStorage
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("age", age);
+    localStorage.setItem("comment", comment);
+  });
+
+  // By default display the content from localStorage
+  $("#name-display").text(localStorage.getItem("name"));
+  $("#email-display").text(localStorage.getItem("email"));
+  $("#age-display").text(localStorage.getItem("age"));
+  $("#comment-display").text(localStorage.getItem("comment"));
